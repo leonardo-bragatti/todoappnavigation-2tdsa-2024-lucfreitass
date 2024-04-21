88c5cbf48564a62db43c997567c01cb55bbeea77
lucfreitass <Luccarvfreitas@gmail.com>
2024-04-21 18:07:36 -0300

    Tarefa Feira

--- App.tsx
+++ App.tsx
@@ -1,3 +1,27 @@
-const App = () => {};
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { Task } from './Screens/ToDo';
 
-export default App;
+import Details from './Screens/Details';
+import ToDo from "./Screens/ToDo";
+
+export type RootStackParamList = {
+    ToDo: undefined;
+    Details: { task: Task };
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function App(): React.JSX.Element {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="ToDo" component={ToDo}/>
+                <Stack.Screen name="Details" component={Details}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+     );
+};
+
+export default App; 
---
+++ Screens/Details.tsx
@@ -0,0 +1,19 @@
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {Text, View} from 'react-native';
+import {RootStackParamList} from '../App';
+
+type DetailsScreenProps = NativeStackScreenProps<RootStackParamList, 'Details'>;
+
+const Details = ({ route }: DetailsScreenProps) => {
+  const { task } = route.params;
+
+  return (
+    <View>
+      <Text>Nome da Tarefa: {task.name}</Text>
+      <Text>Criada em: {task.createdAt.toLocaleTimeString()} No Dia: {task.createdAt.toLocaleDateString()}</Text>
+      <Text>Finalizada: {task.isFinished ? 'Sim' : 'Não'}</Text>
+    </View>
+  );
+};
+
+export default Details;
